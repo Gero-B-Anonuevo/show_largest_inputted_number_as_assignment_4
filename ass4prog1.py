f608e9ec3b3018b13c209eb6b75da75b1d10cc19
Anonuevo <geroanonuevo3@gmail.com>
2024-01-08 13:53:14 +0800

    can determine the highest value but have no interface

--- ass4prog1.py
+++ ass4prog1.py
@@ -7,11 +7,18 @@ first_number = int(input("Enter your first number: "))
 #pop up ask-int for second number
 second_number = int(input("Enter your second number: "))
 #pop up ask-int for third number
-third_number = int(input("Enter your last number: "))
-print(third_number)
+third_number = int(input("Enter your third number: "))
 #and so on can be more than 3 numbers
 #store ng data sa variables
 #solve using set of if-else function
+if first_number > second_number and first_number > third_number:
+    largest = first_number
+elif second_number > first_number and second_number > third_number:
+    largest = second_number
+else:
+    largest = third_number
+
+print(largest)
 #show highest number, ok button and show list in descending order button
 # if ok then end if latter then new pop up showmessage with ok only button
 #remove existing data   
