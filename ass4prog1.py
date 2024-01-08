fe5e5b6bca42960b0a5fcbfa5e5dddedf8618544
Anonuevo <geroanonuevo3@gmail.com>
2024-01-08 13:43:38 +0800

    first input working

---
+++ ass4prog1.py
@@ -0,0 +1,15 @@
+# Ask user to input 3 numbers. Find and print the biggest number using only if-else statement
+
+#pseudocode
+#main interface with welcome text and proceed button
+#pop up ask-int for first number 
+first_number = int(input("Enter your first number: "))
+print(first_number)
+#pop up ask-int for second number
+#pop up ask-int for third number
+#and so on can be more than 3 numbers
+#store ng data sa variables
+#solve using set of if-else function
+#show highest number, ok button and show list in descending order button
+# if ok then end if latter then new pop up showmessage with ok only button
+#remove existing data   
